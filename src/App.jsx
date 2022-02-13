fcfd041f54bd1145e025d89be7dca90d909819d0
Kevin Grande <kevingrande2002@gmail.com>
2022-02-12 21:33:20 -0600

    ADD: fetch data and shows cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Container from "./components/Container/Container";
 import Navbar from "./components/Navbar/Navbar";
 import "./styles/global.scss";
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Container />
     </div>
   );
 }
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import "./Container.scss";
+const Container = () => {
+  const [wantedPeople, setWantedPeople] = useState([]);
+  useEffect(() => {
+    fetchWantedPeople();
+  }, []);
+  const fetchWantedPeople = async () => {
+    const data = await fetch("https://api.fbi.gov/@wanted?pageSize=10&page=1");
+    const response = await data.json();
+    setWantedPeople(response.items);
+  };
+  return (
+    <main className="mainContainer">
+      <div className="mainContainer__title">
+        <h1>Most Wanted</h1>
+      </div>
+      <div className="mainContainer__buttons">
+        <button>All</button>
+        <button>Main</button>
+        <button>Victim</button>
+      </div>
+      <div className="mainContainer__grid">
+        {wantedPeople.map((items) => {
+          return (
+            <div key={items.uid} className="mainContainer__grid--cards">
+              <p>{items.title}</p>
+              <img
+                src={`https://www.fbi.gov/${items.path}/@@images/image`}
+                alt="fbi"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
+};
+
+export default Container;
