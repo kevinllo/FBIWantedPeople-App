452dea08b74922c55ac945b9b8621475b9641455
Kevin Grande <kevingrande2002@gmail.com>
2022-02-13 23:04:01 -0600

    MODIFY: Navbar items

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,10 +7,15 @@ const Navbar = () => {
     <nav className="mainNav">
       <div className="mainNav__items">
         <div className="mainNav__items--logo">
-          <img src={fbiLogo} alt="nav logo" />
+          <a href="https://www.fbi.gov/">
+            <img src={fbiLogo} alt="nav logo" />
+          </a>
         </div>
         <div className="mainNav__items--title">
           <h1>FBI</h1>
+        </div>
+        <div className="mainNav__items--titleDescription">
+          <span>FEDERAL BUREU OF INVESTIGATION</span>
         </div>
       </div>
     </nav>
